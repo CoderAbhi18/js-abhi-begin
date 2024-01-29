4c5f0541de09fa42d9b00af0b0c70abada8c369d
CoderAbhi18 <abhijitsalunkhe18269@gmail.com>
2024-01-29 21:21:44 +0530

    conversion

---
+++ begin/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Abhijit"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33Abc" => NaN(Not An Number)
+// true => 1; false =>0
+
+
+let isLoggendIn = "Abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggendIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "Abhi" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
