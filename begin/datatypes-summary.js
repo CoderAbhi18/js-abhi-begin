e8ee9401b3564df227e263f1905480eb4c64e05f
CoderAbhi18 <abhijitsalunkhe18269@gmail.com>
2024-01-30 20:55:32 +0530

    datatype summary

---
+++ begin/datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive data Type
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 2254653623n
+
+
+
+
+
+// Reference Type (Non Primitive)
+
+// Arrays, Objects, Functions, 
+
+const heros = ["Shaktiman", "Naagraj", "Modi"]
+let myObj = {
+    name: "Abhijit",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros);
