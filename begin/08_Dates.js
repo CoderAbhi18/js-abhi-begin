38c43d8cb8a60a701d1dd72350fee7f4e4c224ef
CoderAbhi18 <abhijitsalunkhe18269@gmail.com>
2024-02-01 13:34:23 +0530

    dates

---
+++ begin/08_Dates.js
@@ -0,0 +1,38 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(typeof myDate);
+
+
+// let myCreateDate = new Date(2023, 0, 23, 5, 23)
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date("2024-01-01")
+let myCreateDate = new Date("01-01-2024")
+
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+    
+})
