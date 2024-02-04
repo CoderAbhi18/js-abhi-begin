f5d39ef2da8d64a002f01c2a915350a4201d0dde
CoderAbhi18 <abhijitsalunkhe18269@gmail.com>
2024-02-04 12:52:40 +0530

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -31,4 +31,32 @@ function loginUserMessage(username = "Abhi"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Abhijit"))
-console.log(loginUserMessage("Abhijit"))
+// console.log(loginUserMessage("Abhijit"))
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000, 400))
+
+const user = {
+    username: "abhijit",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 3000, 100, 900]
+
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondvalue(myNewArray));
+console.log(returnSecondvalue(myNewArray));
