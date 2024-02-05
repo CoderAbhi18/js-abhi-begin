0b4889ffc275fe7551a133451fce1f4984c29f80
CoderAbhi18 <abhijitsalunkhe18269@gmail.com>
2024-02-05 19:58:13 +0530

    scope

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -3,9 +3,48 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a)
+    // console.log("INNER: ", a)
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "Abhijit"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Abhijit"
+    if (username === "Abhijit") {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++++++ INTRESTING +++++++++++++++++++++++++ //
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+const addtwo = function(num){
+    return num + 2
+}
+
+addtwo(5)
