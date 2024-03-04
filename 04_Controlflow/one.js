ee141e3f653d39ee09d7b1e7b833ed3b8d4bcff5
Abhijit <abhijitsalunkhe18269@gmail.com>
2024-03-04 19:51:45 +0530

    one

---
+++ 04_Controlflow/one.js
@@ -0,0 +1,41 @@
+// // if
+
+// // const isUserLoggedIn = true
+// // const tempreature = 45
+
+// // if (tempreature < 40){
+// //     console.log("less than 40");
+// // } else{
+// // console.log("tempreature is greater than 40")
+// // }
+
+// // const score = 200
+
+// // if (score > 100){
+// //     const power = "fly"
+// //     console.log(`user power: ${power}`);
+// // }
+// // console.log(`user power: ${power}`);
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("test");
+// if (balance < 500){
+//     console.log("less than 500");
+// } else if (balance < 650){
+//     console.log("less than 650");
+// } else{
+//     console.log('less than 1200');
+// }
+
+const userLoggedIn =true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInfromEmail = true
+
+if (userLoggedIn && debitcard && 2==2) {
+    console.log("Allow to buy course");
+}
+if (loggedInFromGoogle || loggedInfromEmail || Guestuser){
+    console.log("useer logged in");
+}
